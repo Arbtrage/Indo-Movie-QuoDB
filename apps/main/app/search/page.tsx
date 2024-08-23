942126368c8ee934c4fde3c4864957cc30633a64
Sayantan <sayantann2002@gmail.com>
2024-08-23 22:36:41 +0530

    Fix : Build Errors

--- apps/main/app/search/page.tsx
+++ apps/main/app/search/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "../../components/Header";
 import QuoteCard from "../../components/QuoteCard";
+import { Suspense } from 'react'
+
 
 const page = () => {
     return (
         <div className="relative flex w-full items-center min-h-screen flex-col pt-[80px] overflow-hidden rounded-lg border bg-background md:shadow-xl">
-            <Header />
-            <QuoteCard/>
+            <Suspense>
+                <Header />
+                <QuoteCard />
+            </Suspense>
         </div>
     );
 };
