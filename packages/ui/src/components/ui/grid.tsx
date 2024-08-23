4d2834bdb08908feeec4490ffcbceb32c8c45caf
Sayantan <sayantann2002@gmail.com>
2024-08-23 23:12:58 +0530

    Fix : Updated lib import

--- packages/ui/src/components/ui/grid.tsx
+++ packages/ui/src/components/ui/grid.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useId, useRef, useState } from "react";
 import { motion } from "framer-motion";
 
-import { cn } from "@ui/lib/utils";
+import { cn } from "@ui/libs/utils";
 
 interface GridPatternProps {
     width?: number;
