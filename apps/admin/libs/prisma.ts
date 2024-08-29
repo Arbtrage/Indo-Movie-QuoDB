1390378653162373358439e29175c2880d0a032a
Sayantan <sayantann2002@gmail.com>
2024-08-29 15:23:25 +0530

    Chore : added transaction limit

--- apps/admin/libs/prisma.ts
+++ apps/admin/libs/prisma.ts
@@ -1,5 +1,11 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, Prisma } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    transactionOptions: {
+        isolationLevel: Prisma.TransactionIsolationLevel.Serializable,
+        maxWait: 5000, 
+        timeout: 10000, 
+    },
+});
 
 export default prisma
